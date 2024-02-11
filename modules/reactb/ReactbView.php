201d34ee79668bbf9d5f198839e72e3678d292d1
BitsHost <dadryan26@yahoo.com>
2024-02-11 21:49:36 +0200

    upMVCCopyrightUpdated

--- modules/reactb/ReactbView.php
+++ modules/reactb/ReactbView.php
@@ -1,5 +1,32 @@
 <?php
 
+/*
+ *   Created on Tue Oct 31 2023
+ 
+ *   Copyright (c) 2023 BitsHost
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ *   Here you may host your app for free:
+ *   https://bitshost.biz/
+ */
+
 namespace Reactb;
 
 use Common\Bmvc\BaseView;
@@ -12,7 +39,7 @@ class ReactbView extends BaseView
         $this->startHead($title);
 
 ?>
-<!--  we just get parts from index.html from our react app latest Build from etc/Build -->
+        <!--  we just get parts from index.html from our react app latest Build from etc/Build -->
         <meta charset="utf-8" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Web site created using create-react-app" />
@@ -31,7 +58,7 @@ class ReactbView extends BaseView
             .column {
                 float: left;
                 padding: 10px;
-                height:90%;
+                height: 90%;
                 /* Should be removed. Only for demonstration */
             }
 
@@ -59,18 +86,18 @@ class ReactbView extends BaseView
         <div class="row">
             <div class="column left" style="background-color:#FFA500;">
                 <h2>Column 1</h2>
-               
+
             </div>
             <div id="root" class="column middle" style="background-color:#FF00FF;">
                 <h2>Column 2</h2>
-            
+
             </div>
             <div class="column right" style="background-color:#FFA500;">
                 <h2>Column 3</h2>
-                
+
             </div>
         </div>
-        
+
 <?php
         $this->endBody();
         $this->startFooter();
