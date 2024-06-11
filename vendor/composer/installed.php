a47650aaf447847bb60b24fe108fa23986bb8600
BitsHost <dadryan26@yahoo.com>
2024-06-11 16:51:25 +0300

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'bitshost/upmvc',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '6545aa3a609b9d18d8d2bbb6fa1938589a2fdea3',
+        'reference' => '4a7067314da2470fc4c96cf1533bd02d55ca3506',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'bitshost/upmvc' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '6545aa3a609b9d18d8d2bbb6fa1938589a2fdea3',
+            'reference' => '4a7067314da2470fc4c96cf1533bd02d55ca3506',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
